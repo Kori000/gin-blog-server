cdc148baa697d7193688a7eee1e88f2c8c887ad7
Kori <kexin@korix.top>
2023-09-27 21:50:17 +0800

    feat: use json for show error code msg

---
+++ testdata/1.错误码读取.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"gin-blog-server/models/res"
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
+
+const file = "models/res/err_code.json"
+
+type ErrMap map[res.ErrorCode]string
+
+func main() {
+	byteData, err := os.ReadFile(file)
+	if err != nil {
+		logrus.Error(err)
+		return
+	}
+
+	var errMap = ErrMap{}
+	err = json.Unmarshal(byteData, &errMap)
+	if err != nil {
+		logrus.Error(err)
+		return
+	}
+
+	fmt.Println(errMap[res.SettingsError])
+
+}
